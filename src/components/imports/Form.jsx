03dd81e865124ce829a6bac9e36e54e10f4896a9
MircoRodriguez <githubflip@gmail.com>
2024-12-11 18:31:56 -0300

    18 commit - full landing desing

--- src/components/imports/Form.jsx
+++ src/components/imports/Form.jsx
@@ -41,191 +41,218 @@ export const FormContact = () => {
 
     return (
         <form onSubmit={handleSubmit} className="content-form " noValidate>
-            {/* NOMBRE COMPLETO */}
-            <div>
-                <label htmlFor="nombreCompleto" className="bold-text" aria-label="Nombre del usuario">
-                    Nombre Completo:
-                </label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="nombreCompleto"
-                    name="nombreCompleto"
-                    value={formData.nombreCompleto}
-                    onChange={handleChange}
-                />
-                {errors.nombreCompleto && (
-                    <small className="text-danger" aria-live="assertive">
-                        {errors.nombreCompleto}
-                    </small>
-                )}
-            </div>
+            <p className="campos-obligatorios">(*) Campos obligatorios</p>
 
-            {/* ESTADO */}
-            <div>
-                <label htmlFor="estado" className="bold-text" aria-label="Estado donde vive del usuario">
-                    Estado donde vives:
-                </label>
-                <select className="form-control" id="estado" name="estado" value={formData.estado} onChange={handleChange}>
-                    <option value="">Selecciona un estado</option>
-                    {estadosMexico.map((estado, index) => (
-                        <option key={index} value={estado}>
-                            {estado}
-                        </option>
-                    ))}
-                </select>
-                {errors.estado && (
-                    <small className="text-danger" aria-live="assertive">
-                        {errors.estado}
-                    </small>
-                )}
-            </div>
-
-            {/* ¿CÓMO QUIERES QUE TE CONTACTEMOS? */}
-            <div>
-                <label htmlFor="contactoPreferido" className="bold-text" aria-label="Método de contacto preferido">
-                    ¿Cómo quieres que te contactemos?
-                </label>
-                <select
-                    className="form-control"
-                    id="contactoPreferido"
-                    name="contactoPreferido"
-                    value={formData.contactoPreferido}
-                    onChange={handleChange}
-                >
-                    <option value="">Selecciona una opción</option>
-                    <option value="whatsapp">WhatsApp</option>
-                    <option value="llamada">Llamada Telefónica</option>
-                    <option value="email">Correo Electrónico</option>
-                </select>
-                {errors.contactoPreferido && (
-                    <small className="text-danger" aria-live="assertive">
-                        {errors.contactoPreferido}
-                    </small>
-                )}
-            </div>
-
-            {/* TELÉFONO */}
-            {['whatsapp', 'llamada'].includes(formData.contactoPreferido) && (
-                <div>
-                    <label htmlFor="telefono" className="bold-text" aria-label="Teléfono del usuario">
-                        Teléfono:
+            <div className="form-block-1">
+                {/* NOMBRE COMPLETO */}
+                <div className="input-control">
+                    <label htmlFor="nombreCompleto" className="bold-text" aria-label="Nombre del usuario">
+                        *Nombre Completo:
                     </label>
                     <input
                         type="text"
                         className="form-control"
-                        id="telefono"
-                        name="telefono"
-                        value={formData.telefono}
+                        id="nombreCompleto"
+                        name="nombreCompleto"
+                        value={formData.nombreCompleto}
                         onChange={handleChange}
-                        maxLength="13"
                     />
-                    {errors.telefono && (
+                    {errors.nombreCompleto && (
                         <small className="text-danger" aria-live="assertive">
-                            {errors.telefono}
+                            {errors.nombreCompleto}
                         </small>
                     )}
                 </div>
-            )}
 
-            {/* EMAIL */}
-            {formData.contactoPreferido === 'email' && (
-                <div>
-                    <label htmlFor="email" className="bold-text" aria-label="Correo Electrónico del usuario">
-                        Correo Electrónico:
+                {/* ESTADO */}
+                <div className="input-control">
+                    <label htmlFor="estado" className="bold-text" aria-label="Estado donde vive del usuario">
+                        *Estado donde vives:
                     </label>
-                    <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleChange} />
-                    {errors.email && (
+                    <select className="form-control" id="estado" name="estado" value={formData.estado} onChange={handleChange}>
+                        <option value="">Selecciona un estado</option>
+                        {estadosMexico.map((estado, index) => (
+                            <option key={index} value={estado}>
+                                {estado}
+                            </option>
+                        ))}
+                    </select>
+                    {errors.estado && (
                         <small className="text-danger" aria-live="assertive">
-                            {errors.email}
+                            {errors.estado}
                         </small>
                     )}
                 </div>
-            )}
+            </div>
 
-            {/* CURRICULUM */}
-            <div>
-                <label htmlFor="cvFile" className="bold-text" aria-label="Carga tu curriculum">
-                    Cargar tu CV:
-                </label>
-                <input
-                    type="file"
-                    className="form-control"
-                    id="cvFile"
-                    name="cvFile"
-                    onChange={handleFileChange}
-                    accept=".pdf,.doc,.docx,.jpg,.jpeg,.png"
-                />
-                {errors.cvFile && (
-                    <small className="text-danger" aria-live="assertive">
-                        {errors.cvFile}
-                    </small>
+            <div className="form-block-2">
+                {/* ¿CÓMO QUIERES QUE TE CONTACTEMOS? */}
+                <div className="input-control">
+                    <label htmlFor="contactoPreferido" className="bold-text" aria-label="Método de contacto preferido">
+                        *¿Cómo quieres que te contactemos?
+                    </label>
+                    <select
+                        className="form-control"
+                        id="contactoPreferido"
+                        name="contactoPreferido"
+                        value={formData.contactoPreferido}
+                        onChange={handleChange}
+                    >
+                        <option value="">Selecciona una opción</option>
+                        <option value="whatsapp">WhatsApp</option>
+                        <option value="llamada">Llamada Telefónica</option>
+                        <option value="email">Correo Electrónico</option>
+                    </select>
+                    {errors.contactoPreferido && (
+                        <small className="text-danger" aria-live="assertive">
+                            {errors.contactoPreferido}
+                        </small>
+                    )}
+                </div>
+
+                {/* TELÉFONO */}
+                {['whatsapp', 'llamada'].includes(formData.contactoPreferido) && (
+                    <div className="input-control">
+                        <label htmlFor="telefono" className="bold-text" aria-label="Teléfono del usuario">
+                            *Teléfono:
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="telefono"
+                            name="telefono"
+                            value={formData.telefono}
+                            onChange={handleChange}
+                            maxLength="13"
+                        />
+                        {errors.telefono && (
+                            <small className="text-danger" aria-live="assertive">
+                                {errors.telefono}
+                            </small>
+                        )}
+                    </div>
+                )}
+
+                {/* EMAIL */}
+                {formData.contactoPreferido === 'email' && (
+                    <div className="input-control">
+                        <label htmlFor="email" className="bold-text" aria-label="Correo Electrónico del usuario">
+                            *Correo Electrónico:
+                        </label>
+                        <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleChange} />
+                        {errors.email && (
+                            <small className="text-danger" aria-live="assertive">
+                                {errors.email}
+                            </small>
+                        )}
+                    </div>
                 )}
             </div>
 
-            {/* CHECKBOX PARA MOSTRAR CAMPOS EXTRAS */}
-            <div>
-                <label htmlFor="extraFieldsCheckbox" className="bold-text">
-                    ¿Tienes cédula?
-                </label>
-                <input type="checkbox" id="extraFieldsCheckbox" checked={showExtraFields} onChange={toggleExtraFields} className="form-check-input" />
+            <div className="form-block-3">
+                {/* CURRICULUM */}
+                <div className="input-control">
+                    <label htmlFor="cvFile" className="bold-text" aria-label="Carga tu curriculum">
+                        Cargar tu CV:
+                    </label>
+                    <input
+                        type="file"
+                        className="form-control"
+                        id="cvFile"
+                        name="cvFile"
+                        onChange={handleFileChange}
+                        accept=".pdf,.doc,.docx,.jpg,.jpeg,.png"
+                    />
+                    {errors.cvFile && (
+                        <small className="text-danger" aria-live="assertive">
+                            {errors.cvFile}
+                        </small>
+                    )}
+                </div>
             </div>
 
-            {/* CAMPOS ADICIONALES */}
-            {showExtraFields && (
-                <>
-                    <div>
-                        <label htmlFor="tipoCedula" className="bold-text">
-                            Tipo de Cédula
-                        </label>
-                        <select className="form-control" id="tipoCedula" name="tipoCedula" value={formData.tipoCedula} onChange={handleChange}>
-                            <option value="">Selecciona un tipo</option>
-                            {tiposCedula.map((tipoCedula, index) => (
-                                <option key={index} value={tipoCedula}>
-                                    {tipoCedula}
-                                </option>
-                            ))}
-                        </select>
-                        {errors.tipoCedula && (
-                            <small className="text-danger" aria-live="assertive">
-                                {errors.tipoCedula}
-                            </small>
-                        )}
-                    </div>
+            <div className="form-block-4">
+                {/* CHECKBOX PARA MOSTRAR CAMPOS EXTRAS */}
+                <div className="input-control checkbox-control">
+                    <label htmlFor="extraFieldsCheckbox" className="bold-text">
+                        ¿Tienes cédula?
+                    </label>
+                    <input
+                        type="checkbox"
+                        id="extraFieldsCheckbox"
+                        checked={showExtraFields}
+                        onChange={toggleExtraFields}
+                        className="form-check-input"
+                    />
+                </div>
+            </div>
 
-                    <div>
-                        <label htmlFor="cedula" className="bold-text">
-                            Cédula:
-                        </label>
-                        <input type="text" id="cedula" name="cedula" value={formData.cedula} onChange={handleChange} className="form-control" />
-                        {errors.cedula && (
-                            <small className="text-danger" aria-live="assertive">
-                                {errors.cedula}
-                            </small>
-                        )}
-                    </div>
+            <div className="form-block-5">
+                {/* CAMPOS ADICIONALES */}
+                {showExtraFields && (
+                    <>
+                        {/* TIPO CEDULA */}
+                        <div className="input-control">
+                            <label htmlFor="tipoCedula" className="bold-text">
+                                *Tipo de Cédula
+                            </label>
+                            <select className="form-control" id="tipoCedula" name="tipoCedula" value={formData.tipoCedula} onChange={handleChange}>
+                                <option value="">Selecciona un tipo</option>
+                                {tiposCedula.map((tipoCedula, index) => (
+                                    <option key={index} value={tipoCedula}>
+                                        {tipoCedula}
+                                    </option>
+                                ))}
+                            </select>
+                            {errors.tipoCedula && (
+                                <small className="text-danger" aria-live="assertive">
+                                    {errors.tipoCedula}
+                                </small>
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
 
-                    <div>
-                        <label htmlFor="fechaVencimiento" className="bold-text">
-                            Fecha de vencimiento:
-                        </label>
-                        <input
-                            type="date"
-                            id="fechaVencimiento"
-                            name="fechaVencimiento"
-                            value={formData.fechaVencimiento}
-                            onChange={handleChange}
-                            className="form-control"
-                            min={new Date().toISOString().split('T')[0]}
-                        />
-                        {errors.fechaVencimiento && (
-                            <small className="text-danger" aria-live="assertive">
-                                {errors.fechaVencimiento}
-                            </small>
-                        )}
-                    </div>
-                </>
-            )}
+            <div className="form-block-6">
+                {showExtraFields && (
+                    <>
+                        {/* CEDULA */}
+                        <div className="input-control">
+                            <label htmlFor="cedula" className="bold-text">
+                                *Cédula:
+                            </label>
+                            <input type="text" id="cedula" name="cedula" value={formData.cedula} onChange={handleChange} className="form-control" />
+                            {errors.cedula && (
+                                <small className="text-danger" aria-live="assertive">
+                                    {errors.cedula}
+                                </small>
+                            )}
+                        </div>
+
+                        {/* FECHA VEC CEDULA */}
+                        <div className="input-control">
+                            <label htmlFor="fechaVencimiento" className="bold-text">
+                                *Fecha de vencimiento:
+                            </label>
+                            <input
+                                type="date"
+                                id="fechaVencimiento"
+                                name="fechaVencimiento"
+                                value={formData.fechaVencimiento}
+                                onChange={handleChange}
+                                className="form-control"
+                                min={new Date().toISOString().split('T')[0]}
+                            />
+                            {errors.fechaVencimiento && (
+                                <small className="text-danger" aria-live="assertive">
+                                    {errors.fechaVencimiento}
+                                </small>
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
 
             {/* BOTÓN DE ENVÍO */}
             <div className=" content-envio">
--- src/components/imports/Section4.jsx
+++ src/components/imports/Section4.jsx
@@ -1,20 +1,39 @@
+import ImgSection4 from '../../assets/images/Imagen section 4.webp';
 import { FormContact } from './Form';
 
 export const Section4 = () => {
     return (
         <section>
             <div>
-                <h2>Deja tus datos y busca la oportunidad de emprender con MetLife</h2>
+                <div className="section-4-titulo">
+                    <h2 className="bold-text">Deja tus datos y emprende con MetLife</h2>
+                </div>
 
-                <div>
-                    <div>
+                <div className="section-4-container">
+                    <div className="section-4-2">
                         <FormContact />
                     </div>
 
-                    <div>
-                        <img src="" alt="" />
+                    <div className="section-4-3">
+                        <div className="section-4-3-1">
+                            <img
+                                src={ImgSection4}
+                                alt="CEO de VitaMet en traje, invitandote a dejar tus datos en el formulario."
+                                className="img-section-4"
+                                loading="lazy"
+                            />
+                        </div>
+
+                        <div className="section-4-3-2">
+                            <h2 className="light-text">Arturo Elizondo</h2>
+                            <p className="light-text-curve">Ceo VitaMet</p>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="hr-final-section-4">
+                <hr />
             </div>
         </section>
     );
