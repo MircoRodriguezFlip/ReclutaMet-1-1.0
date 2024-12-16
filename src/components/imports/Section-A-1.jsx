dbebc88934116d7a2729921bab8a180b4e01d9cc
MircoRodriguez <githubflip@gmail.com>
2024-12-16 14:13:58 -0300

    23 commit - full maqueta 1.3 / politicas page

--- src/components/imports/Section-A-1.jsx
+++ src/components/imports/Section-A-1.jsx
@@ -6,12 +6,12 @@ export const SectionA1 = () => {
         <section>
             <div className="section-A1-container">
                 <div className="section-1-1 textos-section-A1">
-                    <h1 className="bold-text">¡Impulsa tu Carrera con VitaMet!</h1>
+                    <h1 className="bold-text">¡Impulsa tu Carrera con MetLife!</h1>
 
                     <img src={MetLifeIcon} alt="Icono de MetLife bajo el titulo principal" className="logo-metlife-main" loading="lazy" />
 
                     <p className="light-text">
-                        Si ya cuentas con tu Cédula de Agente <strong>MetLife</strong>, estás listo para alcanzar nuevas alturas en tu desarollo
+                        Si ya cuentas con tu Cédula de Agente <strong>MetLife</strong>, estás listo para alcanzar nuevas alturas en tu desarrollo
                         profesional.
                     </p>
 
@@ -29,7 +29,7 @@ export const SectionA1 = () => {
                     </div>
 
                     <div className="section-1-2-2">
-                        <h2 className="light-text">Rocio Olivoso</h2>
+                        <h2 className="light-text">Roció Olivos</h2>
                         <p className="light-text-curve">Asesora Profesional de Seguros</p>
                     </div>
                 </div>
